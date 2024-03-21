a231f7a44b3791300d82e493c8de82e54bcd2e3b
jwellman13 <46513002+jwellman13@users.noreply.github.com>
2024-03-21 13:19:30 -0700

    Slime Death fixed

--- Assets/Scripts/slimeDie.cs
+++ Assets/Scripts/slimeDie.cs
@@ -2,20 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
 
 public class slimeDie : MonoBehaviour
 
 {
-    
-void OnCollisionEnter2D (Collision2D col)
-{
-if (col.gameObject.tag.Equals ("Bullet"))
-{
-Destroy (col.gameObject);
-Destroy (gameObject);
-} 
-if (col.gameObject.tag.Equals("Player")){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-}
- }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag.Equals("Bullet"))
+        {
+            SlimeDieServerRpc();
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    [ServerRpc]
+    void SlimeDieServerRpc()
+    {
+        Destroy(gameObject);
+        var netObj = GetComponent<NetworkObject>();
+        netObj.Despawn();
+
+    }
 }
